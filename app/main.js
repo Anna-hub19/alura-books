d821c1f3baeee40433e663efabf5ff331fcc42d9
Anna Beatriz Bastos <127196700+Anna-hub19@users.noreply.github.com>
2023-06-28 20:15:52 -0300

    Add files via upload

---
+++ app/main.js
@@ -0,0 +1,94 @@
+let livros = []
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosDaAPI()
+
+async function getBuscarLivrosDaAPI() {
+    const res = await fetch(endpointDaAPI)
+    livros = await res.json()
+    livros = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
+}
+
+const elementoParaInserirLivros = document.getElementById('livros')
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = ''
+    elementoParaInserirLivros.innerHTML = ''
+    listaDeLivros.forEach(livro => {
+        // let disponibilidade = verificarDisponibilidadeDoLivro(livro)
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel' 
+        elementoParaInserirLivros.innerHTML += `
+        <div class="livro">
+        <img class="${disponibilidade}" src="${livro.imagem}"
+          alt="${livro.alt}" />
+        <h2 class="livro__titulo">
+          ${livro.titulo}
+        </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
+      </div>
+        `
+    })
+}
+
+// function verificarDisponibilidadeDoLivro(livro) {
+//   if (livro.quantidade > 0 ){
+//     return 'livro__imagens'
+//   } else {
+//     return 'livros__imagens indisponivel'
+//   }
+// }
+
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : FiltrarPorCategoria(categoria)
+    exibirOsLivrosNaTela(livrosFiltrados)
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
+    }
+}
+
+function FiltrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
+}
+
+function aplicarDesconto(livros) {
+  const desconto = 0.3
+  livrosComDesconto = livros.map(livro => {
+      return {...livro, preco: livro.preco - (livro.preco * desconto)}
+  })
+  return livrosComDesconto
+}
+
+let btnOdenarPorPreco = document.getElementById('btnOrdenarPorPreco')
+btnOdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco)
+
+function ordenarLivrosPorPreco() {
+    let lirvosOrdenados = livros.sort((a, b) => a.preco - b.preco)
+    exibirOsLivrosNaTela(lirvosOrdenados)
+}
+
+function calcularValorTotalDeLivrosDisponiveis(livros) {
+  return livros.reduce((acc, livro) => acc + livro.preco, 0).toFixed(2)
+}
